692f0b73f752c9743814cfb8c51d7c1319f2f39f
anytarsier67 <42589471+anytarseir67@users.noreply.github.com>
2021-08-28 19:12:07 -0400

    Update __init__.py

--- guilded_webhook/__init__.py
+++ guilded_webhook/__init__.py
@@ -5,90 +5,106 @@ from typing import List
 
 class Embed:
     def __init__(self, *, title: str=None, description: str=None, url: str=None, color: int=None, timestamp: datetime=None) -> None:
-        self.title = title
-        self.description = description
-        self.url = url
-        self.color = int(color)
-        self.fields = []
-        self.author = {}
-        self.footer = {}
-        self.timestamp = timestamp
-        self.image = {}
-        #self.video = {}
-        self.thumbnail = {}
+        self._title = title
+        self._description = description
+        self._url = url
+        self._color = int(color)
+        self._fields = []
+        self._author = {}
+        self._footer = {}
+        self._timestamp = timestamp
+        self._image = {}
+        #self._video = {}
+        self._thumbnail = {}
 
     def add_field(self, *, title: str, value: str, inline: bool=False) -> None:
-        self.fields.append({"name": title, "value": value, "inline": inline})
+        self._fields.append({"name": title, "value": value, "inline": inline})
 
     def set_author(self, *, name: str=None, url: str=None, icon_url: str=None) -> None:
         if name:
-            self.author["name"] = name
+            self._author["name"] = name
         if url:
-            self.author["url"] = url
+            self._author["url"] = url
         if icon_url:
-            self.author["iconUrl"] = icon_url
+            self._author["iconUrl"] = icon_url
 
     def set_footer(self, *, text: str, icon_url: str=None) -> None:
-        self.footer["text"] = text
+        self._footer["text"] = text
         if icon_url:
-            self.footer["iconUrl"] = icon_url
+            self._footer["iconUrl"] = icon_url
 
     def set_image(self, url: str, height: int=None, width: int=None) -> None:
-        self.image["url"] = url
+        self._image["url"] = url
         if height:
-            self.image["height"] = height
+            self._image["height"] = height
         if width:
-            self.image["width"] = width
+            self._image["width"] = width
 
     # def set_video(self, url: str, height: int=None, width: int=None) -> None:
-    #     self.video["url"] = url
+    #     self._video["url"] = url
     #     if height:
-    #         self.video["height"] = height
+    #         self._video["height"] = height
     #     if width:
-    #         self.video["width"] = width
+    #         self._video["width"] = width
 
     def set_thumbnail(self, url: str, height: int=None, width: int=None) -> None:
-        self.thumbnail["url"] = url
+        self._thumbnail["url"] = url
         if height:
-            self.thumbnail["height"] = height
+            self._thumbnail["height"] = height
         if width:
-            self.thumbnail["width"] = width
+            self._thumbnail["width"] = width
 
     def _to_dict(self) -> dict:
         data = {}
-        data["title"] = self.title
-        data["description"] = self.description
-        data["url"] = self.url
-        data["color"] = self.color
-        if len(self.fields) != 0:
-            data["fields"] = self.fields
-        if self.author != {}:
-            data["author"] = self.author
-        if self.footer != {}:
-            data["footer"] = self.footer
-        if isinstance(self.timestamp, datetime):
-            data["timestamp"] = self.timestamp.isoformat()
-        if self.image != {}:
-            data["image"] = self.image
-        # if self.video != {}:
-        #     data["video"] = self.video
-        if self.thumbnail != {}:
-            data["thumbnail"] = self.thumbnail
+        data["title"] = self._title
+        data["description"] = self._description
+        data["url"] = self._url
+        data["color"] = self._color
+        if len(self._fields) != 0:
+            data["fields"] = self._fields
+        if self._author != {}:
+            data["author"] = self._author
+        if self._footer != {}:
+            data["footer"] = self._footer
+        if isinstance(self._timestamp, datetime):
+            data["timestamp"] = self._timestamp.isoformat()
+        if self._image != {}:
+            data["image"] = self._image
+        # if self._video != {}:
+        #     data["video"] = self._video
+        if self._thumbnail != {}:
+            data["thumbnail"] = self._thumbnail
         return data
 
 class Webhook:
     def __init__(self, url: str) -> None:
-        self.url = url
+        self._url = url
+
+    def __repr__(self) -> str:
+        return self._url
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, (AsyncWebhook, Webhook)):
+            return self._url == o._url
+        return False
 
     def send(self, *, content: str, embeds: List[Embed]) -> None:
         if isinstance(embeds, Embed):
             embeds = [embeds]
         embeds = [embed._to_dict() for embed in embeds]
-        requests.post(self.url, json={'content': content, 'embeds': embeds})
+        requests.post(self._url, json={'content': content, 'embeds': embeds})
 
 class AsyncWebhook:
     def __init__(self, url: str) -> None:
-        self.url = url
+        self._url = url
+
+    def __repr__(self) -> str:
+        return self._url
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, (AsyncWebhook, Webhook)):
+            return self._url == o._url
+        return False
 
     async def send(self, *, content: str, embeds: List[Embed]) -> None:
         if isinstance(embeds, Embed):
@@ -96,5 +112,5 @@ class AsyncWebhook:
         embeds = [embed._to_dict() for embed in embeds]
         print({'content': content, 'embeds': embeds})
         async with aiohttp.ClientSession() as session:
-            async with session.post(self.url, json={'content': content, 'embeds': embeds}):
-                pass
+            async with session.post(self._url, json={'content': content, 'embeds': embeds}):
+                return
